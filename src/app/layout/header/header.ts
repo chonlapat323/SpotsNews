1268a4730a8dbade8b70d8b373b16cb38efe3fb4
chonlapat323 <cschonlapat@gmail.com>
2025-06-10 12:29:34 +0700

    feat: implement dark/light theme toggle

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnInit {
   menuOpen = false;
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const theme = localStorage.getItem('theme');
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
+    }
+  }
+
+  toggleTheme(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const html = document.documentElement;
+      html.classList.toggle('dark');
+
+      if (html.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+      } else {
+        localStorage.setItem('theme', 'light');
+      }
+    }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // สร้างชุดสีใหม่โดยอ้างอิงจาก CSS Variable
+        background: "var(--color-background)",
+        surface: "var(--color-surface)",
+        primary: "var(--color-primary)",
+      },
+      textColor: {
+        // สร้างชุดสีตัวอักษรใหม่
+        "text-primary": "var(--color-text-primary)",
+        "text-secondary": "var(--color-text-secondary)",
+      },
+    },
   },
   plugins: [],
 };
